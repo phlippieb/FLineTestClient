a77648fc8dc810e2baca55ba6551ad85325f1bf9
phlippieb <phlippie.bosman@gmail.com>
2019-09-24 08:18:11 +0200

    Add basic colors

---
+++ Sources/FLineTestClient/colors.swift
@@ -0,0 +1,16 @@
+enum ANSIColors: String {
+    case none = "\u{001B}[0;0m"
+    case black = "\u{001B}[0;30m"
+    case red = "\u{001B}[0;31m"
+    case green = "\u{001B}[0;32m"
+    case yellow = "\u{001B}[0;33m"
+    case blue = "\u{001B}[0;34m"
+    case magenta = "\u{001B}[0;35m"
+    case cyan = "\u{001B}[0;36m"
+    case white = "\u{001B}[0;37m"
+}
+
+func + (left: ANSIColors, right: String) -> String {
+    return left.rawValue + right
+}
+
